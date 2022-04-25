8e4504a73856ae0b85c1ce138eb2cc3215d67b33
ryrc <1004034712@qq.com>
2022-04-25 22:04:42 +0800

    Changes

--- src/com/ry/jdatastruct/graph/Graph.java
+++ src/com/ry/jdatastruct/graph/Graph.java
@@ -1,11 +1,8 @@
 package com.ry.jdatastruct.graph;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class Graph {
+public class Graph {//有向图
     private class Node{
         private String label;
 
@@ -40,7 +37,6 @@ public class Graph {
         }
 
         adjacencyList.get(fromNode).add(toNode);
-        adjacencyList.get(toNode).add(fromNode);
     }
 
     public void print(){
@@ -73,6 +69,151 @@ public class Graph {
             return;
         }
         adjacencyList.get(fromNode).remove(toNode);
+    }
+
+    public void traverseDepthFirstRec(String root){
+        var node = nodes.get(root);
+        if (node == null){
+            return;
+        }
+        traverseDepthFirstRec(nodes.get(root), new HashSet<>());
+    }
+    private void traverseDepthFirstRec(Node root, Set<Node> visited){
+        System.out.println(root);
+        visited.add(root);
+
+        for (var node : adjacencyList.get(root)){
+            if (!visited.contains(node)){
+                traverseDepthFirstRec(node, visited);
+            }
+        }
+    }
+
+    public void traverseDepthFirst(String root){
+        var node = nodes.get(root);
+        if (node == null){
+            return;
+        }
+
+        Set<Node> visited = new HashSet<>();
+        Stack<Node> stack = new Stack<>();
+        stack.push(node);
+
+        while(!stack.isEmpty()){
+            var current = stack.pop();
+
+            if (visited.contains(current)){
+                continue;
+            }
+            System.out.println(current);
+            visited.add(current);
+
+            for (var neighbour : adjacencyList.get(current)){
+                if (!visited.contains(neighbour)){
+                    stack.push(neighbour);
+                }
+            }
+        }
+    }
+
+    public void traverseBreadthFirst(String root){
+        var node = nodes.get(root);
+        if (node == null){
+            return;
+        }
+        Set<Node> visited = new HashSet<>();
+
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(node);
+        while (!queue.isEmpty()){
+            var current = queue.remove();
+
+            if (visited.contains(current)){
+                continue;
+            }
+            System.out.println(current);
+            visited.add(current);
+
+            for (var neighbour : adjacencyList.get(current)){
+                if (!visited.contains(neighbour)){
+                    queue.add(neighbour);
+                }
+            }
+        }
+    }
+
+    public List<String> topologicalSort(){
+        Stack<Node> stack = new Stack<>();
+        Set<Node> visited = new HashSet<>();
+
+        for (var node : nodes.values()){
+            topologicalSort(node, visited, stack);
+        }
+
+        List<String> sorted = new ArrayList<>();
+        while(!stack.empty()){
+            sorted.add(stack.pop().label);
+        }
+
+        return sorted;
+    }
+
+    private void topologicalSort(
+            Node node, Set<Node> visited, Stack<Node> stack){
+
+        if (visited.contains(node)){
+            return;
+        }
+        visited.add(node);
+
+        for (var neighbour : adjacencyList.get(node)){
+            topologicalSort(neighbour, visited, stack);
+        }
+
+        stack.push(node);
+    }
+
+    //nodes [a(a), b(b), c(c)]
+    //ad...[a(b, c), b(c), c()]
+    public boolean hasCycle(){
+        Set<Node> all = new HashSet<>();
+        all.addAll(nodes.values());//[a, b, c]
+
+        Set<Node> visiting = new HashSet<>();
+        Set<Node> visited = new HashSet<>();
+
+        while (!all.isEmpty()){
+            var current = all.iterator().next();
+            if (hasCycle(current, all, visiting, visited)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasCycle(
+            Node node, Set<Node> all, Set<Node> visiting, Set<Node> visited){
+
+        all.remove(node);//[b , c]
+        visiting.add(node);//[a]
+
+        for (var neighbour : adjacencyList.get(node)){
+            if (visited.contains(neighbour)){
+                continue;
+            }
+            if (visiting.contains(neighbour)){
+                return true;
+            }
+
+            if (hasCycle(neighbour, all, visiting, visited)){
+                return true;
+            }
+        }
+
+        visiting.remove(node);
+        visited.add(node);
+
+        return false;
     }
 
 
--- src/com/ry/jdatastruct/graph/Main.java
+++ src/com/ry/jdatastruct/graph/Main.java
@@ -3,5 +3,13 @@ package com.ry.jdatastruct.graph;
 public class Main {
     public static void main(String[] args) {
 
+        var graph = new Graph();
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addEdge("A", "B");
+        graph.addEdge("B", "C");
+        graph.addEdge("A", "C");
+        System.out.println(graph.hasCycle());
     }
 }
