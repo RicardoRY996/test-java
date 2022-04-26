e8a494df61fe2862eb10db2c8145d56a753e7aab
ryrc <1004034712@qq.com>
2022-04-26 22:19:49 +0800

    Changes

--- src/com/ry/jdatastruct/graph/Main.java
+++ src/com/ry/jdatastruct/graph/Main.java
@@ -2,14 +2,29 @@ package com.ry.jdatastruct.graph;
 
 public class Main {
     public static void main(String[] args) {
-
-        var graph = new Graph();
+//
+//        var graph = new Graph();
+//        graph.addNode("A");
+//        graph.addNode("B");
+//        graph.addNode("C");
+//        graph.addEdge("A", "B");
+//        graph.addEdge("B", "C");
+//        graph.addEdge("A", "C");
+//        System.out.println(graph.hasCycle());
+        var graph = new WeightedGraph();
         graph.addNode("A");
         graph.addNode("B");
         graph.addNode("C");
-        graph.addEdge("A", "B");
-        graph.addEdge("B", "C");
-        graph.addEdge("A", "C");
-        System.out.println(graph.hasCycle());
+        graph.addNode("D");
+
+        graph.addEdge("A", "B", 3);
+        graph.addEdge("B", "D", 4);
+        graph.addEdge("C", "D", 5);
+        graph.addEdge("A", "C", 1);
+        graph.addEdge("B", "C", 2);
+
+        var tree = graph.getMinimumSpanningTree();
+        System.out.println(tree);
+
     }
 }
---
+++ src/com/ry/jdatastruct/graph/Path.java
@@ -0,0 +1,17 @@
+package com.ry.jdatastruct.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Path {
+    private List<String> nodes = new ArrayList<>();
+
+    public void add(String node){
+        nodes.add(node);
+    }
+
+    @Override
+    public String toString() {
+        return nodes.toString();
+    }
+}
